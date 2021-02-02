4ae203663149f3427d648eeb51c4dd466f20b59e
pablo <pablo.arandarod@gmail.com>
2021-02-02 17:12:46 +0100

    Minor modification

--- communication/server.py
+++ communication/server.py
@@ -44,7 +44,7 @@ def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept()
+        conn, addr = server.accept()    # Will wait until a new connection has been received
         thread = threading.Thread(target=handle_client, args = (conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
