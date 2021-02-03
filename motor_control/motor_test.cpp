4509f77a3869a7f2b357cde8af475f3203d06c0e
pablo <pablo.arandarod@gmail.com>
2021-02-03 16:11:20 +0100

    Taken out the named parameters in functions

--- motor_control/motor_test.cpp
+++ motor_control/motor_test.cpp
@@ -13,6 +13,8 @@ int main() {
 
     // Create MatrixIOBus object for hardware communication
 	matrix_hal::MatrixIOBus bus;
+    int speed {0};
+    int dir {0};
     
     // Initialize bus and exit program if error occurs
     if (!bus.Init())
@@ -24,22 +26,34 @@ int main() {
 	// Set gpio to use MatrixIOBus bus
 	gpio.Setup(&bus);
 
-    Motor left(PWM = TB6612_LEFT_MOTOR_PWMB, IN1 = TB6612_LEFT_MOTOR_BIN1, IN2 = TB6612_LEFT_MOTOR_BIN2, BUS = bus, GPIO = gpio);
-    Motor right(PWM = TB6612_RIGHT_MOTOR_PWMB, IN1 = TB6612_RIGHT_MOTOR_AIN1, IN2 = TB6612_RIGHT_MOTOR_AIN2, BUS = bus, GPIO = gpio);
+    Motor left(TB6612_LEFT_MOTOR_PWMB, TB6612_LEFT_MOTOR_BIN1, TB6612_LEFT_MOTOR_BIN2, bus, gpio);
+    Motor right(TB6612_RIGHT_MOTOR_PWMB, TB6612_RIGHT_MOTOR_AIN1, TB6612_RIGHT_MOTOR_AIN2, bus, gpio);
 
-    left.setMotorSpeedDirection(speed = 50, dir = 1);
+    speed = 50;
+    dir = 1;
 
-    std::cin.get();
-
-    left.setMotorSpeedDirection(speed = 50, dir = 0);
-
-    std::cin.get();
-
-    right.setMotorSpeedDirection(speed = 50, dir = 1);
+    left.setMotorSpeedDirection(speed, dir);
 
     std::cin.get();
 
-    right.setMotorSpeedDirection(speed = 50, dir = 0);
+    speed = 50;
+    dir = 0;
+
+    left.setMotorSpeedDirection(speed, dir);
+
+    std::cin.get();
+
+    speed = 50;
+    dir = 1;
+
+    right.setMotorSpeedDirection(speed , dir);
+
+    std::cin.get();
+
+    speed = 50;
+    dir = 0;
+
+    right.setMotorSpeedDirection(speed , dir);
 
     std::cin.get();
 
