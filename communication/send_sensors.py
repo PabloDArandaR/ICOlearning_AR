fbd234105c0f71ccf9886c78a79197d1c598a29a
pablo <pablo.arandarod@gmail.com>
2021-02-03 20:35:44 +0100

    Turned object to be sent to a dictionary

--- communication/send_sensors.py
+++ communication/send_sensors.py
@@ -8,14 +8,35 @@ board = Client()
 board.create_socket()
 board.create_connection()
 
-dict_data = dict()
-dict_data
+_to_send = dict()
+_to_send['accel_x'] = 0.0
+_to_send['accel_y'] = 0.0
+_to_send['accel_z'] = 0.0
+_to_send['gyro_x'] = 0.0
+_to_send['gyro_y'] = 0.0
+_to_send['gyro_z'] = 0.0
+_to_send['mag_x'] = 0.0
+_to_send['mag_y'] = 0.0
+_to_send['mag_z'] = 0.0
+_to_send['yaw'] = 0.0
+_to_send['pitch'] = 0.0
+_to_send['roll'] = 0.0
+
 
 while True:
-    _to_send = sensors.imu.read()
-    print(type(_to_send))
-    print(_to_send)
-    print(_to_send.accel_x)
+    _reading = sensors.imu.read()
+    _to_send['accel_x'] = _reading.accel_x
+    _to_send['accel_y'] = _reading.accel_y
+    _to_send['accel_z'] = _reading.accel_z
+    _to_send['gyro_x'] = _reading.gyro_x
+    _to_send['gyro_y'] = _reading.gyro_y
+    _to_send['gyro_z'] = _reading.gyro_z
+    _to_send['mag_x'] = _reading.mag_x
+    _to_send['mag_y'] = _reading.mag_y
+    _to_send['mag_z'] = _reading.mag_z
+    _to_send['yaw'] = _reading.yaw
+    _to_send['pitch'] = _reading.pitch
+    _to_send['roll'] = _reading.roll
     
     x = input()
     
