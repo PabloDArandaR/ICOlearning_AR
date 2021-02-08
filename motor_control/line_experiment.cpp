e13ae725d1203c0e0e6f19fa01771074172eea46
pablo <pablo.arandarod@gmail.com>
2021-02-08 17:05:45 +0100

    Small modification

--- motor_control/line_experiment.cpp
+++ motor_control/line_experiment.cpp
@@ -36,8 +36,8 @@ int main() {
     Motor left(TB6612_LEFT_MOTOR_PWMB, TB6612_LEFT_MOTOR_BIN1, TB6612_LEFT_MOTOR_BIN2, &gpio);
     Motor right(TB6612_RIGHT_MOTOR_PWMA, TB6612_RIGHT_MOTOR_AIN1, TB6612_RIGHT_MOTOR_AIN2, &gpio);
 
-    left.setMotorSpeedDirection(&gpio, 100, 0);
-    right.setMotorSpeedDirection(&gpio, 100, 0);
+    left.setMotorSpeedDirection(&gpio, 100, 1);
+    right.setMotorSpeedDirection(&gpio, 100, 1);
 
     auto start = std::chrono::high_resolution_clock::now();
     auto end   = std::chrono::high_resolution_clock::now();
