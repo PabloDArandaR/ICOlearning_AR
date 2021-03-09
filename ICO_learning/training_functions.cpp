101e000ed7333381cee66b10930a214602a4c561
pablo <pablo.arandarod@gmail.com>
2021-03-09 15:16:54 +0100

    Added functions to train more signals. Mixed pitch and roll is not complete

--- ICO_learning/training_functions.cpp
+++ ICO_learning/training_functions.cpp
@@ -155,3 +155,280 @@ void train_roll(Motor left, Motor right, matrix_hal::IMUData imu_data, float wei
     file.close();
 }
 
+void train_pitch(Motor left, Motor right, matrix_hal::IMUData imu_data, float weight_pitch[], float learning_rate, int speed[], matrix_hal::GPIOControl gpio, matrix_hal::IMUSensor imu_sensor)
+{
+    //Variables required for the different calculations:
+    float sampling_time;
+    float pitch_data[10];
+    int mean_pitch {0};
+    int dir[2];
+    float reflex {0};
+    float diff {0};
+    float extra[2];
+    std::ofstream file;
+    auto finish = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
+    file.open("evolution.txt", std::ios_base::app);
+
+    //Stabilize measurements part:
+    
+    
+    for (int i = 0; i < sizeof(pitch_data)/sizeof(pitch_data[0]); i++){
+        pitch_data[i] = 0;
+    }
+
+    for (int i = 0; i < sizeof(pitch_data)/sizeof(pitch_data[0]); i++){
+        // Overwrites imu_data with new data from IMU sensor
+        imu_sensor.Read(&imu_data);
+        pitch_data[i] = imu_data.pitch;
+    }
+
+    mean_pitch = mean(pitch_data);
+
+    sampling_time = 10;
+
+    //Learning part
+    while (true){
+
+
+        // Record start time
+        start = std::chrono::high_resolution_clock::now();
+
+        diff = 0;
+        dir[0] = 1;
+        dir[1] = 1;
+        extra[0] = 0;
+        extra[1] = 0;
+
+        // Overwrites imu_data with new data from IMU sensor
+        imu_sensor.Read(&imu_data);
+
+        pitch_and_add(imu_data.pitch, pitch_data);
+        mean_pitch = mean(pitch_data);
+
+        if (abs(mean_pitch) > 50.0f){
+            break;
+        }
+
+        //Reflex signal calculation -> Depends on the pitch angle sign to see which weight is updated
+
+        if (mean_pitch > 3.0f){
+            diff = mean_pitch - reflex;
+            weight_pitch[0] += learning_rate*mean_pitch*diff;
+        
+            reflex = mean_pitch;
+        }
+        else if (mean_pitch < -3.0f){
+            diff = mean_pitch - reflex;
+            weight_pitch[1] += learning_rate*mean_pitch*diff;
+
+            reflex = mean_pitch;
+        }
+        else {
+            reflex = 0;
+        }
+
+        extra[0] = weight_pitch[0]*mean_pitch + reflex;
+        extra[1] = weight_pitch[1]*mean_pitch + reflex;
+
+
+        if (extra[0] < 0){
+            extra[0] = -extra[0];
+            dir[0] = 0;
+        }
+        else{
+            dir[0] = 1;
+        }
+        if ((extra[0] + speed[0]) > 100){
+            extra[0] = 100-speed[0];
+        }
+
+
+        if (extra[1] < 0){
+            extra[1] = -extra[1];
+            dir[1] = 0;
+        }
+        else{
+            dir[1] = 1;
+        }
+        if ((extra[1] + speed[1]) > 100){
+            extra[1] = 100-speed[1];
+        }
+
+        std::cout << "speed[0] = " << speed[0]+extra[0] << "    speed[1] = " << speed[1]+extra[1] << std::endl;
+        std::cout << "dir[0]   =  " << dir[0] << "    dir[1] =  " << dir[1] << std::endl;
+
+        left.setMotorSpeedDirection(&gpio, speed[0] + extra[0], dir[0]);
+        right.setMotorSpeedDirection(&gpio, speed[1] + extra[1], dir[1]);
+
+        std::cout << "pitch angle: " << mean_pitch << std::endl;
+        std::cout << "Weight[0] = " << weight_pitch[0] << "    Weight[1] = " << weight_pitch[1] << std::endl;
+        
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Record end time
+        finish = std::chrono::high_resolution_clock::now();
+
+        std::cout << "Time spent: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << " miliseconds" << std::endl;
+
+        while (std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() < sampling_time)
+        {
+            finish = std::chrono::high_resolution_clock::now();
+        }
+
+        file << weight_pitch[0] << "," << weight_pitch[1] << "," << imu_data.pitch << "," << mean_pitch << "," << speed[0]+extra[0] << "," << speed[1]+extra[1] << "," << reflex << "," << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << std::endl;
+
+        std::cout << "-------------------------------------------------------------------------------------------" << std::endl;
+
+    }
+    
+    std::cout << "Finished learning round!" << std::endl;
+    
+    
+    left.setMotorSpeedDirection(&gpio, 0, dir[0]);
+    right.setMotorSpeedDirection(&gpio, 0, dir[1]);
+
+    file.close();
+}
+
+void train_pitch&roll(Motor left, Motor right, matrix_hal::IMUData imu_data, float weight_pitch[], float weight_roll[], float learning_rate, int speed[], matrix_hal::GPIOControl gpio, matrix_hal::IMUSensor imu_sensor)
+{
+    //Variables required for the different calculations:
+    float sampling_time;
+    float pitch_data[10];
+    int mean_pitch {0};
+    int mean_roll {0};
+    int dir[2];
+    float reflex {0};
+    float diff {0};
+    float extra[2];
+    std::ofstream file;
+    auto finish = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
+    file.open("evolution.txt", std::ios_base::app);
+
+    //Stabilize measurements part:
+    
+    
+    for (int i = 0; i < sizeof(pitch_data)/sizeof(pitch_data[0]); i++){
+        pitch_data[i] = 0;
+        roll_data[i] = 0;
+    }
+
+    for (int i = 0; i < sizeof(pitch_data)/sizeof(pitch_data[0]); i++){
+        // Overwrites imu_data with new data from IMU sensor
+        imu_sensor.Read(&imu_data);
+        pitch_data[i] = imu_data.pitch;
+        roll_data[i] = imu_data.roll;
+    }
+
+    mean_pitch = mean(pitch_data);
+    mean_roll  = mean(roll_data);
+
+    sampling_time = 10;
+
+    //Learning part
+    while (true){
+
+
+        // Record start time
+        start = std::chrono::high_resolution_clock::now();
+
+        diff = 0;
+        dir[0] = 1;
+        dir[1] = 1;
+        extra[0] = 0;
+        extra[1] = 0;
+
+        // Overwrites imu_data with new data from IMU sensor
+        imu_sensor.Read(&imu_data);
+
+        pitch_and_add(imu_data.pitch, pitch_data);
+        pitch_and_add(imu_data.roll, roll_data);
+        mean_pitch = mean(pitch_data);
+        mean_pitch = mean(roll_data);
+
+        if ((abs(mean_pitch) > 50.0f) | (abs(mean_roll) > 50.0f){
+            break;
+        }
+
+        //Reflex signal calculation -> Depends on the pitch angle sign to see which weight is updated
+
+        if (mean_pitch > 3.0f){
+            diff = mean_pitch - reflex;
+            weight_pitch[0] += learning_rate*mean_pitch*diff;
+        
+            reflex = mean_pitch;
+        }
+        else if (mean_pitch < -3.0f){
+            diff = mean_pitch - reflex;
+            weight_pitch[1] += learning_rate*mean_pitch*diff;
+
+            reflex = mean_pitch;
+        }
+        else {
+            reflex = 0;
+        }
+
+        extra[0] = weight_pitch[0]*mean_pitch + reflex;
+        extra[1] = weight_pitch[1]*mean_pitch + reflex;
+
+
+        if (extra[0] < 0){
+            extra[0] = -extra[0];
+            dir[0] = 0;
+        }
+        else{
+            dir[0] = 1;
+        }
+        if ((extra[0] + speed[0]) > 100){
+            extra[0] = 100-speed[0];
+        }
+
+
+        if (extra[1] < 0){
+            extra[1] = -extra[1];
+            dir[1] = 0;
+        }
+        else{
+            dir[1] = 1;
+        }
+        if ((extra[1] + speed[1]) > 100){
+            extra[1] = 100-speed[1];
+        }
+
+        std::cout << "speed[0] = " << speed[0]+extra[0] << "    speed[1] = " << speed[1]+extra[1] << std::endl;
+        std::cout << "dir[0]   =  " << dir[0] << "    dir[1] =  " << dir[1] << std::endl;
+
+        left.setMotorSpeedDirection(&gpio, speed[0] + extra[0], dir[0]);
+        right.setMotorSpeedDirection(&gpio, speed[1] + extra[1], dir[1]);
+
+        std::cout << "pitch angle: " << mean_pitch << std::endl;
+        std::cout << "Weight[0] = " << weight_pitch[0] << "    Weight[1] = " << weight_pitch[1] << std::endl;
+        
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Record end time
+        finish = std::chrono::high_resolution_clock::now();
+
+        std::cout << "Time spent: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << " miliseconds" << std::endl;
+
+        while (std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() < sampling_time)
+        {
+            finish = std::chrono::high_resolution_clock::now();
+        }
+
+        file << weight_pitch[0] << "," << weight_pitch[1] << "," << imu_data.pitch << "," << mean_pitch << "," << speed[0]+extra[0] << "," << speed[1]+extra[1] << "," << reflex << "," << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << std::endl;
+
+        std::cout << "-------------------------------------------------------------------------------------------" << std::endl;
+
+    }
+    
+    std::cout << "Finished learning round!" << std::endl;
+    
+    
+    left.setMotorSpeedDirection(&gpio, 0, dir[0]);
+    right.setMotorSpeedDirection(&gpio, 0, dir[1]);
+
+    file.close();
+}
+
+
