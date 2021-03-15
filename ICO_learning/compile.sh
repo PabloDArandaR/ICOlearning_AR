7028dde6d509341ede1cf53a966ebfc622e3c39b
pablo <pablo.arandarod@gmail.com>
2021-03-15 11:25:08 +0100

    Modified compile.sh to add the function files

--- ICO_learning/compile.sh
+++ ICO_learning/compile.sh
@@ -1,2 +1,2 @@
-g++ main.cpp ../motor_control/motor_class.cpp -lmatrix_creator_hal -o ico_1
+g++ main.cpp ../motor_control/motor_class.cpp functions/calibrate.cpp function/templates.hpp function/variety.cpp -lmatrix_creator_hal -o ico_1
 
