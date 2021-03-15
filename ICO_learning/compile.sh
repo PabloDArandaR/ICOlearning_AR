eaa514c706d507736f90849300c928ce887e3628
pablo <pablo.arandarod@gmail.com>
2021-03-15 11:26:55 +0100

    Small error in the path of the files of compile.sh

--- ICO_learning/compile.sh
+++ ICO_learning/compile.sh
@@ -1,2 +1,2 @@
-g++ main.cpp ../motor_control/motor_class.cpp functions/calibrate.cpp function/templates.hpp function/variety.cpp -lmatrix_creator_hal -o ico_1
+g++ main.cpp ../motor_control/motor_class.cpp functions/calibrate.cpp functions/templates.hpp functions/variety.cpp -lmatrix_creator_hal -o ico_1
 
