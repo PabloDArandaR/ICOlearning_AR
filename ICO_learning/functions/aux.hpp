8a515ed83d14f2707a9c4e1c169903f9de3e5ab8
pablo <pablo.arandarod@gmail.com>
2021-03-30 12:32:50 +0200

    Check evolution of weights after each iteration

--- ICO_learning/functions/aux.hpp
+++ ICO_learning/functions/aux.hpp
@@ -1,4 +1,5 @@
-
+#ifndef _AUX_GUARD
+#define _AUX_GUARD
 
 void WeightUpdate1(float , float , float , float [], float * , bool *);
 void WeightUpdate2(float , float , float , float [], float * , bool *);
@@ -6,3 +7,5 @@ void WeightUpdate3(float , float , float , float [], float * , bool *);
 
 void SpeedSaturation1(float * , float , const int , int );
 void SpeedSaturation2(float * , float , const int , int );
+
+#endif _AUX_GUARD
