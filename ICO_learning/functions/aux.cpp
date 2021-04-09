41ca4c43b622c65c09fde6bd0595a9973d0b2154
pablo <pablo.arandarod@gmail.com>
2021-04-09 14:48:34 +0200

    Signal pitch changed to abs(pitch)

--- ICO_learning/functions/aux.cpp
+++ ICO_learning/functions/aux.cpp
@@ -179,6 +179,51 @@ void WeightUpdateB2(float mean_pitch, float mean_roll, float limit, float learni
     *reflex = mean;
 }
 
+void WeightUpdateB3(float mean_pitch, float mean_roll, float limit, float learning_rate, float weight_roll[], float weight_pitch[], float * reflex, bool *reflex_ON)
+{
+    float diff {.0f};
+    float mean {.0f};
+
+    // If the value of the signal surpasses the reflex signal threshold, the update will be commited.
+    // If not, there won't be any update
+    // Possible interference in the learning process due to the sign of both angles
+    if (abs(mean_roll) > limit){
+        mean += mean_roll;
+    }
+    if (abs(mean_pitch) > limit){
+        mean += mean_pitch;
+    }
+
+    if ((abs(mean_roll) > limit) & (abs(mean_pitch) > limit))
+    {
+        *reflex_ON = false;
+    }
+    else
+    {
+        *reflex_ON = true;
+    }
+
+    diff = mean - *reflex;
+
+
+    // Update of the weight using the given learning rule
+
+    if (mean_roll > 0)
+    {
+        weight_roll[0] += learning_rate*mean_roll*diff;
+    }
+    else
+    {
+        weight_roll[1] += learning_rate*mean_roll*diff;
+    }
+
+    weight_pitch[0] -= learning_rate*mean_pitch*diff;
+    weight_pitch[1] -= learning_rate*mean_pitch*diff;
+
+    *reflex = mean;
+}
+
+
 void SpeedSaturation1(float * extra, float limit, int speed[], int dir[])
 {
     for (int i = 0; i < sizeof(extra)/sizeof(extra[0]); i++)
