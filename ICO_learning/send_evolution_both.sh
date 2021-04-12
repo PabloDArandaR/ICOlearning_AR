5cf41dbfbef12fe5a65071e0daa89efdd0fceb92
pablo <pablo.arandarod@gmail.com>
2021-04-12 19:33:09 +0200

    Small modification in both send

--- ICO_learning/send_evolution_both.sh
+++ ICO_learning/send_evolution_both.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-file_name=evolution_both.txt
+file_name=evolution_both.csv
 
 current_time=$(date "+%d-%m_%H.%M")
 
-new_file_name=evolution_$current_time.txt
+new_file_name=evolution_$current_time.csv
 
 cp $file_name $new_file_name
 
