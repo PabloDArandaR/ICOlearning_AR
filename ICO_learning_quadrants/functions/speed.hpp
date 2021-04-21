36abdd874e148c8cef469d8af995ec0b40e18bdd
pablo <pablo.arandarod@gmail.com>
2021-04-21 23:06:26 +0200

    Quadrants

--- ICO_learning_quadrants/functions/speed.hpp
+++ ICO_learning_quadrants/functions/speed.hpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+// Interfaces with IMU sensor
+#include "matrix_hal/imu_sensor.h"
+// Holds data from IMU sensor
+#include "matrix_hal/imu_data.h"
+// Communicates with MATRIX device
+#include "matrix_hal/matrixio_bus.h"
+
 float * ExtraCalculation(float, float, int [], float [], float [], float , int []);
 int CheckQuadrant(float, float);
 void WeightUpdateRobot(float , float, float [], float [], float, int, float *);
