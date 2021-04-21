172bf3bd754aabd078c785025a8a98af5fb96c02
pablo <pablo.arandarod@gmail.com>
2021-04-21 23:32:35 +0200

    Quadrants

--- ICO_learning_quadrants/functions/aux.cpp
+++ ICO_learning_quadrants/functions/aux.cpp
@@ -311,8 +311,8 @@ void InitialFilter(float * roll, float * pitch, matrix_hal::IMUData imu_data, ma
 
         // Calculate both roll and pitch based on the new reading
         //floa LowPassFilter(float,                 float,  float, float);
-        roll = LowPassFilter(sampling_time/1000.0f, cutoff, *roll,   imu_data.roll);
-        pitch = LowPassFilter(sampling_time/1000.0f, cutoff, *pitch, imu_data.pitch);
+        roll = LowPassFilter(sampling_time/1000.0f, cutoff, roll,   imu_data.roll);
+        pitch = LowPassFilter(sampling_time/1000.0f, cutoff, pitch, imu_data.pitch);
 
         //Capture the final moment of the iteration
         end = std::chrono::high_resolution_clock::now();
