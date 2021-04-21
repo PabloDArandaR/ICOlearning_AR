46ac43b58c639b86d2e00d3107ed58414a8c538e
pablo <pablo.arandarod@gmail.com>
2021-04-21 22:38:02 +0200

    Quadrants

--- ICO_learning_quadrants/functions/speed.hpp
+++ ICO_learning_quadrants/functions/speed.hpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
-float * ExtraCalculation(float, float, float [], float [], float []);
+float * ExtraCalculation(float, float, int [], float [], float [], float , int []);
 int CheckQuadrant(float, float);
 void WeightUpdateRobot(float , float, float [], float [], float, int, float *);
