a14401d9bb9d59d341f0ef7a75b9c81664fad688
pablo <pablo.arandarod@gmail.com>
2021-04-22 11:51:50 +0200

    Quadrants

--- ICO_learning_quadrants/functions/speed.hpp
+++ ICO_learning_quadrants/functions/speed.hpp
@@ -11,6 +11,7 @@
 float * ExtraCalculation(float, float, int [], float [], float [], float , int []);
 float ExtraL(float pitch, float roll, int speed[], float weight_roll[], float weight_pitch[], float limit, int dir[]);
 float ExtraR(float pitch, float roll, int speed[], float weight_roll[], float weight_pitch[], float limit, int dir[]);
+void SpeedSaturation1(float *, float, int [], int []);
 int CheckQuadrant(float, float);
 void WeightUpdateRobot(float , float, float [], float [], float, int, float *);
 void InitialFilter(float *, float *, matrix_hal::IMUData, matrix_hal::GPIOControl, matrix_hal::IMUSensor, float, float);
