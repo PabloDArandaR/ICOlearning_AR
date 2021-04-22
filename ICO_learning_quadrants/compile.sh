b74a1709cde164cea0a196867df10c9467af5eb7
pablo <pablo.arandarod@gmail.com>
2021-04-22 10:42:30 +0200

    Quadrants

--- ICO_learning_quadrants/compile.sh
+++ ICO_learning_quadrants/compile.sh
@@ -1,4 +1,4 @@
-g++ main.cpp ../motor_control/motor_class.cpp functions/speed.cpp -lmatrix_creator_hal -o ico_1
+g++ main.cpp ../motor_control/motor_class.cpp functions/training_functions.cpp functions/speed.cpp -lmatrix_creator_hal -o ico_1
 
 echo "To execute the program, execute the following command: "
 echo ""
