2493468d39e6c90e320347afcef7f89c77ebe8a0
pablo <pablo.arandarod@gmail.com>
2021-04-22 10:10:11 +0200

    Quadrants

--- ICO_learning_quadrants/functions/speed.cpp
+++ ICO_learning_quadrants/functions/speed.cpp
@@ -1,12 +1,5 @@
 #include "calibrate.hpp"
 
-// Interfaces with IMU sensor
-#include "matrix_hal/imu_sensor.h"
-// Holds data from IMU sensor
-#include "matrix_hal/imu_data.h"
-// Communicates with MATRIX device
-#include "matrix_hal/matrixio_bus.h"
-
 void SpeedSaturation1(float * extra, float limit, int speed[], int dir[])
 {
     for (int i = 0; i < sizeof(extra)/sizeof(extra[0]); i++)
