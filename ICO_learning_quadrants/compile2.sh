f89384cb39ca96cfad23ab98763ec8a9e07b4c7c
pablo <pablo.arandarod@gmail.com>
2021-04-29 14:34:44 +0200

    Small change in quadrants

--- ICO_learning_quadrants/compile2.sh
+++ ICO_learning_quadrants/compile2.sh
@@ -1,4 +1,4 @@
-g++ main2.cpp ../motor_control/motor_class.cpp functions/training_functions.cpp functions/speed.cpp -lmatrix_creator_hal -o ico_1
+g++ main2.cpp ../motor_control/motor_class.cpp functions/training_functions.cpp functions/speed.cpp -lmatrix_creator_hal -o ico_2
 
 echo "To execute the program, execute the following command: "
 echo ""
