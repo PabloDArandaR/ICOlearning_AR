7561d178ca51d453015efee9dfabea87cc65ac54
pablo <pablo.arandarod@gmail.com>
2021-05-16 16:16:02 +0200

    Running file

--- run/compile.sh
+++ run/compile.sh
@@ -1,4 +1,4 @@
-g++ bin/main.cpp ../motor_control/motor_class.cpp bin/aux.cpp -lmatrix_creator_hal -o run
+g++ bin/main.cpp ../motor_control/motor_class.cpp -lmatrix_creator_hal -o run
 
 echo "To execute the program, execute the following command: "
 echo ""
